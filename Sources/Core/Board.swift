78d3a897a61a7e1765bf7095102ac47fee0bd545
Connor Vickers <connor@Connors-MacBook-Pro.local>
2016-06-08 13:46:26 -0500

    implimented board to set move and get position

--- Sources/Core/Board.swift
+++ Sources/Core/Board.swift
@@ -1,9 +1,27 @@
 public class Board {
     let size: Int
+    var markers: [String]
+    var moves: [Int]
+    var move: Int
+    
     public init(size: Int = 3){
         self.size = size
+        markers = [String]()
+        for position in 1...(size*size) {
+            markers.append(String(position))
+        }
+        moves = [Int]()
+        move = 0
     }
     public func getSize()-> Int{
         return size
+    }
+    public func move(position: Int, marker: String){
+        markers[position] = marker
+        moves.append(position)
+        move += 1
+    }
+    public func getPosition(position: Int) -> String{
+        return markers[position]
     }
 }
--- Sources/Specs/BoardSpec.swift
+++ Sources/Specs/BoardSpec.swift
@@ -6,4 +6,9 @@ class BoardSpec: Group {
         let board = Board();
 		assert.that(board.getSize(), equals: 3, message: "default board size should be 3")
 	}
+    let setMoveTest = Test { assert in
+        let board = Board();
+        board.move(position: 3, marker: "X")
+        assert.that(board.getPosition(position:3), equals: "X", message: "board should set move and get position")
+    }
 }
