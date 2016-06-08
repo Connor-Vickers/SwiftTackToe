5e3096b725d1d1b660193f58a09ce9181bb27edb
Connor Vickers <connor@Connors-MacBook-Pro.local>
2016-06-08 15:42:28 -0500

    implimented detecting win functinality

--- Sources/Core/Board.swift
+++ Sources/Core/Board.swift
@@ -24,4 +24,63 @@ public class Board {
     public func getPosition(position: Int) -> String{
         return markers[position]
     }
+    public func isWin() -> Bool {
+        var winPossibilities = [[String]]()
+
+        let diagonals = getDiagonals()
+        winPossibilities.append(diagonals.forwardDiagonal)
+        winPossibilities.append(diagonals.backwardDiagonal)
+        
+        winPossibilities += getRows()
+        winPossibilities += getColumns()
+        
+        //search all winPossibilities for win
+        for possibility in winPossibilities {
+            var win = true
+            let marker = possibility[0]
+            for position in possibility {
+                if position != marker {
+                    win = false
+                }
+            }
+            if win {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func getDiagonals() -> (forwardDiagonal: [String], backwardDiagonal: [String]){
+        var forwardDiagonal = [String]()
+        var backwardDiagonal = [String]()
+        for i in 0..<size {
+            backwardDiagonal.append(markers[i*size+i])
+            forwardDiagonal.append(markers[i*size+(size-i-1)])
+        }
+        return (forwardDiagonal, backwardDiagonal)
+    }
+    
+    func getRows() -> [[String]] {
+        var rows = [[String]]()
+        for rowNumber in 0..<size {
+            var row = [String]()
+            for column in 0..<size {
+                row.append(markers[rowNumber*size+column])
+            }
+            rows.append(row)
+        }
+        return rows
+    }
+    
+    func getColumns() -> [[String]] {
+        var columns = [[String]]()
+        for columnNumber in 0..<size {
+            var column = [String]()
+            for row in 0..<size {
+                column.append(markers[row*size+columnNumber])
+            }
+            columns.append(column)
+        }
+        return columns
+    }
 }
--- Sources/Specs/BoardSpec.swift
+++ Sources/Specs/BoardSpec.swift
@@ -11,4 +11,36 @@ class BoardSpec: Group {
         board.move(position: 3, marker: "X")
         assert.that(board.getPosition(position:3), equals: "X", message: "board should set move and get position")
     }
+    let rowWinTest = Test { assert in
+        let board = Board();
+        board.move(position: 3, marker: "X")
+        board.move(position: 4, marker: "X")
+        board.move(position: 5, marker: "X")
+        assert.that(board.isWin(), equals: true, message: "board should should reconise row win")
+    }
+    let colWinTest = Test { assert in
+        let board = Board();
+        board.move(position: 2, marker: "X")
+        board.move(position: 5, marker: "X")
+        board.move(position: 8, marker: "X")
+        assert.that(board.isWin(), equals: true, message: "board should should reconise col win")
+    }
+    let backslashWinTest = Test { assert in
+        let board = Board();
+        board.move(position: 0, marker: "X")
+        board.move(position: 4, marker: "X")
+        board.move(position: 8, marker: "X")
+        assert.that(board.isWin(), equals: true, message: "board should should reconise backslash win")
+    }
+    let forwardslashWinTest = Test { assert in
+        let board = Board();
+        board.move(position: 2, marker: "X")
+        board.move(position: 4, marker: "X")
+        board.move(position: 6, marker: "X")
+        assert.that(board.isWin(), equals: true, message: "board should should reconise forwardslash win")
+    }
+    let noWinTest = Test { assert in
+        let board = Board();
+        assert.that(board.isWin(), equals: false, message: "board should should reconise when no win occurs")
+    }
 }
