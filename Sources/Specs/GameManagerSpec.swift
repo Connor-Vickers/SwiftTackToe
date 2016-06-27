8180f195122701db47e88083950e1605f961f08c
Connor Vickers <connor@Connors-MacBook-Pro.local>
2016-06-27 15:52:23 -0500

    added win and tie output tests

--- Sources/Specs/GameManagerSpec.swift
+++ Sources/Specs/GameManagerSpec.swift
@@ -6,7 +6,7 @@ struct GameManagerSpec: Group {
     static var game: Game = StandardTTT(board: GameManagerSpec.board)
     static var player1: Player = PlayerMock(marker: "X")
     static var player2: Player = PlayerMock(marker: "O")
-    static var io: IO = TestIO(inputs:[])
+    static var io: TestIO = TestIO(inputs:[])
     static var gameManager: GameManager = GameManager(player1: player1, player2: player2, io: io, board: board, game: game)
 
     
@@ -21,11 +21,25 @@ struct GameManagerSpec: Group {
 
     }
 
-    let rowWinTest = Test { assert in
-        
+    let outputResultsOfTieTest = Test { assert in
+        board.move(position: 0, marker: "X")
+        board.move(position: 1, marker: "O")
+        board.move(position: 2, marker: "X")
         board.move(position: 3, marker: "X")
-        board.move(position: 4, marker: "X")
+        board.move(position: 4, marker: "O")
         board.move(position: 5, marker: "X")
-        assert.that(game.isWin())
+        board.move(position: 6, marker: "O")
+        board.move(position: 7, marker: "X")
+        board.move(position: 8, marker: "O")
+        gameManager.start()
+        assert.that(io.getOutputAsASingleString(), equals: "\nX|O|X\n_____\nX|O|X\n_____\nO|X|O\nGame was a Tie")
+    }
+
+    let outputResultsOfWinTest = Test { assert in
+        board.move(position: 0, marker: "O")
+        board.move(position: 1, marker: "O")
+        board.move(position: 2, marker: "O")
+        gameManager.start()
+        assert.that(io.getOutputAsASingleString(), equals: "\nO|O|O\n_____\n3|4|5\n_____\n6|7|8\nO won!\n")
     }
 }
